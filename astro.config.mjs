95f72b6605dbf5ac0f0b9f41d7b1c4a19801ed4d
Sai Samineni <samineni@amazon.com>
2025-07-03 11:41:14 -0700

    Fix typo in dependency

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import content from "@astrojs/content";
+import content from "@astrojs/content-collections";
 
 export default defineConfig({
   integrations: [mdx(), sitemap(), content()],
