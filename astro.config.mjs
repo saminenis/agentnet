6759f9bba6b46d2d7582b7046089899368e1e0c8
Sai Samineni <samineni@amazon.com>
2025-07-04 08:04:04 -0700

    fixing build issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
+import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   integrations: [mdx(), sitemap()],
 });
-
